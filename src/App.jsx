1ec9ccdd2b03057cfef2c03e0aaa7c43e685e662
Robert <bobgravity1@gmail.com>
2022-12-17 15:58:23 -0500

    with custom config set up

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,25 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
-        <h1 className="text-blue-500">
-          this is a project starter set up with tailwind, emmet, and
-          intellisense
-        </h1>
-        <button className="bg-blue-400 py-3 bx-5 mt-5 focus:outline-none border-0 hover:bg-blue-300 transition-all .2s">
-          press me
-        </button>
+      <div className="flex gap-8 justify-between">
+        <div className="bg-blue-200 text-left big:bg-red-200 p-8 rounded-xl">
+          <h2 className="text-orange-400  text-sm  big:text-red-600">
+            this is a project starter set up with tailwind, emmet, and
+            intellisense
+          </h2>
+          <button className="bg-blue-400 text-sm  text-white py-3 bx-5 mt-5 focus:outline-none border-0 hover:bg-blue-400/70  hover:scale-110  transition-all .2s">
+            press me
+          </button>
+        </div>
+        <div className="bg-blue-200 text-left  rounded-xl p-8 big:bg-red-200">
+          <h2 className="text-orange-400 text-sm big:text-red-600">
+            this is a project starter set up with tailwind, emmet, and
+            intellisense
+          </h2>
+          <button className="bg-blue-400 text-sm text-white py-3 bx-5 mt-5 focus:outline-none border-0 hover:scale-110 hover:bg-blue-400/70  transition-all .2s">
+            press me
+          </button>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,34 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      "2xl": [
+        "24px",
+        {
+          letterSpacing: "-0.01em",
+        },
+      ],
+      xl: [
+        "20px",
+        {
+          letterSpacing: "-0.01em",
+        },
+      ],
+      sm: [
+        "16px",
+        {
+          letterSpacing: ".2em",
+        },
+      ],
+    },
+    screens: {
+      big: "820px",
+    },
+    extend: {
+      colors: {
+        apple: "red",
+      },
+    },
   },
   plugins: [],
 };
